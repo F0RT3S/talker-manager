ed0fb314dc150ed68b48d55ad770f1e108cfe82f
felipe <fortes.felipe95@gmail.com>
2022-10-04 16:17:50 -0300

    Requisito 01

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -10,6 +13,17 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  try {
+    const pathData = path.resolve(__dirname, './talker.json');
+    const data = JSON.parse(await fs.readFile(pathData, 'utf-8'));
+    const response = data.length > 0 ? data : [];
+    res.status(200).json(response);
+  } catch (error) {
+    console.error(`Erro na leitura do arquivo: ${error}`);
+  }
 });
 
 app.listen(PORT, () => {
